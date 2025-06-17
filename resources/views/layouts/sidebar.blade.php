5f9b0cc77b46f847ea5288a9b5d9ccf1c775962f
New7ech.code <New7ech.code@gmail.com>
2025-06-18 00:11:10 +0100

    les correction

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -34,25 +34,25 @@
                     <div class="collapse" id="demande">
                     <ul class="nav nav-collapse">
                         <li>
-                            <a href="{{ route('admin.articles.create') }}">
+                            <a href="{{ route('articles.create') }}">
                                 <i class="fas fa-cart-plus"></i>
                                 <p>Ajouter un article</p>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('admin.articles.index') }}">
+                            <a href="{{ route('articles.index') }}">
                                 <i class="fas fa-edit"></i>
                                 <p>Gérer les articles</p>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('admin.categories.index') }}">
+                            <a href="{{ route('categories.index') }}">
                                 <i class="fas fa-tags"></i> {{-- Changed icon --}}
                                 <p>Catégories</p>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('admin.emplacements.index') }}">
+                            <a href="{{ route('emplacements.index') }}">
                                 <i class="fas fa-map-marker-alt"></i> {{-- Changed icon --}}
                                 <p>Emplacements</p>
                             </a>
@@ -71,13 +71,13 @@
                     <div class="collapse" id="fournisseur">
                     <ul class="nav nav-collapse">
                         <li>
-                            <a href="{{ route('admin.fournisseurs.create') }}">
+                            <a href="{{ route('fournisseurs.create') }}">
                                 <i class="fas fa-user-plus"></i> {{-- Changed icon --}}
                                 <p>Ajouter un fournisseur</p>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('admin.fournisseurs.index') }}">
+                            <a href="{{ route('fournisseurs.index') }}">
                                 <i class="fas fa-users-cog"></i> {{-- Changed icon --}}
                                 <p>Gérer les fournisseurs</p>
                             </a>
@@ -115,13 +115,13 @@
                     <div class="collapse" id="facture">
                     <ul class="nav nav-collapse">
                         <li>
-                            <a href="{{ route('admin.factures.create') }}">
+                            <a href="{{ route('factures.create') }}">
                                 <i class="fas fa-file-invoice-dollar"></i> {{-- Changed icon --}}
                                 <p>Créer une facture</p>
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('admin.factures.index') }}">
+                            <a href="{{ route('factures.index') }}">
                                 <i class="fas fa-folder-open"></i> {{-- Changed icon --}}
                                 <p>Gérer les factures</p>
                             </a>
@@ -166,12 +166,12 @@
                             <div class="collapse" id="users-submenu">
                                 <ul class="nav nav-collapse">
                                 <li>
-                                    <a href="{{ route('admin.users.index') }}">
+                                    <a href="{{ route('users.index') }}">
                                         <p>Liste des Utilisateurs</p>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="{{ route('admin.users.create') }}">
+                                    <a href="{{ route('users.create') }}">
                                         <p>Créer un Utilisateur</p>
                                     </a>
                                 </li>
@@ -187,12 +187,12 @@
                             <div class="collapse" id="roles-submenu">
                                 <ul class="nav nav-collapse">
                                 <li>
-                                    <a href="{{ route('admin.roles.index') }}">
+                                    <a href="{{ route('roles.index') }}">
                                         <p>Liste des Rôles</p>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="{{ route('admin.roles.create') }}">
+                                    <a href="{{ route('roles.create') }}">
                                         <p>Créer un Rôle</p>
                                     </a>
                                 </li>
@@ -208,12 +208,12 @@
                             <div class="collapse" id="permissions-submenu">
                                 <ul class="nav nav-collapse">
                                 <li>
-                                    <a href="{{ route('admin.permissions.index') }}">
+                                    <a href="{{ route('permissions.index') }}">
                                         <p>Liste des Permissions</p>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="{{ route('admin.permissions.create') }}">
+                                    <a href="{{ route('permissions.create') }}">
                                         <p>Créer une Permission</p>
                                   </a>
                               </li>
